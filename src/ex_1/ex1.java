db813af38baa75ea294cb2ebda9653b598f30939
LeeSooJin <gowl0918@gmail.com>
2024-04-19 17:15:16 +0900

    미니

---
+++ src/ex_1/ex1.java
@@ -0,0 +1,10 @@
+package ex_1;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		System.out.println("아수진");
+
+	}
+
+}
---
+++ src/ex_2/ex2.java
@@ -0,0 +1,10 @@
+package ex_2;
+
+public class ex2{
+
+	public static void main(String[] args) {
+		System.out.println("아수진");
+
+	}
+
+}
